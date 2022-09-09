1ca2ecf83eda625a64604ea0853535d2c3153b96
terkrub <terkrub9@gmail.com>
2022-09-09 14:56:48 +1200

    add chatpage

---
+++ lib/Model/Message.dart
@@ -0,0 +1,8 @@
+class Message {
+  final String text;
+  final String senderUsername;
+  final String receiverUsername;
+  final bool sendbyme;
+
+  Message(this.text, this.senderUsername, this.receiverUsername, this.sendbyme);
+}
